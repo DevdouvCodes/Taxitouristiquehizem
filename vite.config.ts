ddb620f49284009980183bc2e5497f6e87768265
devdouvCodes <fattouha@proton.me>
2025-11-11 11:51:04 +0100

    vite conf

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
